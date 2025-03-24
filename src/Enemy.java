b245b61f2b4e990f911fb3b7babac41ee87ffaf4
Vince <putiri.v@northeastern.edu>
2025-03-24 16:14:10 -0400

    update the enemy constructor

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,5 +1,6 @@
+import com.google.gson.annotations.SerializedName;
 
-
+import java.awt.image.BufferedImage;
 
 
 /**
@@ -8,43 +9,72 @@
  */
 public class Enemy extends AbstractObstacle {
   private String name;
+  private boolean active; // make a boolean
+
+  @SerializedName("affects_target")
+  private boolean affectsTarget; // make a boolean
+
+  @SerializedName("affects_player")
+  private boolean affectsPlayer; // make a boolean
+
+  private String solution;
+  private String value; // make an int
   private String description;
-  private String answer;
-  private boolean solved = false;
-  private int health;
-  private int attackDamage;
-  private int hurtEnemy;
-  private String item; // Is this the correct type?
+  private String effects;
+  private int damage;  // make an int
+  private String target;
 
+  @SerializedName("can_attack")
+  private boolean canAttack; // make a boolean
 
-  public Enemy(String name, String description, String answer, boolean solved, int attackDamage) {
-    super(name, description, answer);
-    this.solved = solved;
-    this.health = 100;
-    this.attackDamage = attackDamage;
-    // add item to drop
-    // add a drop item function
+  private String attack;
+  private BufferedImage picture; //https://stackoverflow.com/questions/34072052/is-it-possible-to-add-an-image-png-as-an-attribute-of-a-java-class
+
+  public Enemy(String name, String active, String affects_target, String affects_player,
+               String solution, String value, String description, String effects, String damage,
+               String target, String can_attack, String attack, BufferedImage picture) {
+    super(name, description, solution);
+    this.active = booleanChecker(active);
+    this.affectsTarget = booleanChecker(affects_target);
+    this.affectsPlayer = booleanChecker(affects_player);
+    this.solution = solution;
+    this.value = value;
+    this.description = description;
+    this.effects = effects;
+    this.damage = Integer.parseInt(damage);
+    this.target = target;
+    this.canAttack = booleanChecker(can_attack);
+    this.attack = attack;
+    this.picture = picture;
   }
 
-  /**
-   * Returns current health.
-   */
-  @Override
-  public int getHealth() {
-    return this.health;
+  private boolean booleanChecker(String s) {
+    return s.equals("true");
   }
 
-  /**
-   * This addresses an attack on the enemy.
-   */
-  @Override
-  public void decrementHealth() {
-    this.health = health - 25;
-    System.out.println("You've hit the enemy!");
-    if (this.health <= 0) {
-      this.solved = true;
-      System.out.println("The enemy has been defeated and is going to sleep!");
-    }
+
+
+  // * Returns current health.
+  // */
+  //@Override
+  //public int getHealth() {
+  //  return this.health;
+  //}
+
+
+
+
+
+  // * This addresses an attack on the enemy.
+  // */
+  //@Override
+  //public void decrementHealth() {
+  //  this.health = health - 25;
+  //  System.out.println("You've hit the enemy!");
+  //  if (this.health <= 0) {
+  //    this.solved = true;
+  //    System.out.println("The enemy has been defeated and is going to sleep!");
+  //  }
   }
 
-}
+
--- src/RoomObstacles.java
+++ src/RoomObstacles.java
@@ -1,14 +1,60 @@
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * The room obstacles class manages the passing of key information about any relevant
  * puzzles or enemies to the Room class upon instantiation. Examples of information passing
  * include creation, removal, checks on whether an obstacle has been cleared during the game play.
  */
-public class RoomObstacles {
+public class RoomObstacles implements IHolder {
 
   private HashMap<String, IObstacle> obstacles;
 
 
+  /**
+   * Check if the bag have enough storage.
+   * if true add the item in to the storage list.
+   *
+   * @param item
+   */
+  @Override
+  public boolean addItem(IItem item) {
+    return false;
+  }
 
+  /**
+   * Check if the item is in the holder.
+   * if true remove and return the item.
+   *
+   * @param name
+   */
+  @Override
+  public void removeItem(String name) {
+
+  }
+
+  /**
+   * Check if the item is in the holder.
+   *
+   * @return Item object
+   */
+  @Override
+  public <T> List<T> getItem() {
+    return List.of();
+  }
+
+  /**
+   * set weight limit.
+   *
+   * @param limit
+   */
+  @Override
+  public void setWeightLimit(int limit) {
+
+  }
+
+  @Override
+  public String toString() {
+    return "";
+  }
 }
