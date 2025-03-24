088340e656ed0e32f247f0dbc3b79340fe415446
vputiri2024 <putiri.v@northeastern.edu>
2025-03-24 12:47:52 -0400

    Delete src/Green directory

--- src/Green/IElements.java
+++
@@ -1,5 +0,0 @@
-package Green;
-
-public interface IElements {
-  String getDescription();
-}
--- src/Green/RoomContents.java
+++
@@ -1,7 +0,0 @@
-package Green;
-
-import java.util.Map;
-
-public class RoomContents implement IHolder {
-  private Map<String, IElements> contents;
-}
