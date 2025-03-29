df74121eb6e17efc507350fe81895bf3650ae351
Vince <putiri.v@northeastern.edu>
2025-03-29 12:50:56 -0400

    added professors content to MainTest2t

--- src/gamedriver/MainTest2.java
+++ src/gamedriver/MainTest2.java
@@ -2,8 +2,10 @@ package gamedriver;
 
 import com.google.gson.Gson;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.StringReader;
 
 import gamedriver.avatar.AvatarController;
 import gamedriver.game.Game;
@@ -13,6 +15,19 @@ import gamedriver.obstacle.IObstacle;
 public class MainTest2 {
 
   public static void main(String[] args) {
+
+    // smoke tests - first send synthetic data via a string
+    String s = "Sir Mix-A-Lot\nT NOTEBOOK\nN\nT HAIR CLIPPERS\nT KEY\nD NOTEBOOK\nQuit";
+    BufferedReader stringReader = new BufferedReader(new StringReader(s));
+    GameEngineApp gameEngineApp = new GameEngineApp("./resources/align_quest_game_elements.json", stringReader, System.out);
+    gameEngineApp.start();
+
+
+    // Next, comment the above and uncomment this to do some ad-hoc testing by hand via System.in
+    // GameEngineApp gameEngineApp = new GameEngineApp("./resources/museum.json", new InputStreamReader(System.in), System.out);
+    // gameEngineApp.start();
+
+    }
 
     FileReader reader;
     {
